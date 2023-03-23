ac488c1846c9a3c7dcf9f50f6d1c8fbb3e36c837
alejandroodom <113924039+alejandroodom@users.noreply.github.com>
2023-03-23 19:59:59 +0100

    Ejercicio1 terminado y ejercicio2 hecho la clase tablero(hice 2 commits antes pero no se guardaron porque los hice desde IntelliJ en vez de hacerlo en el desktop)

--- Ejercicio2/src/main/java/org/example/Tablero.java
+++ Ejercicio2/src/main/java/org/example/Tablero.java
@@ -13,5 +13,18 @@
 package org.example;
 
 public class Tablero{
+
+    private static int DIMENSION = 30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    public void leerEstadoActual(){
+        estadoActual = new int[DIMENSION][DIMENSION];
+        for (int i = 0; i < DIMENSION; i++){
+            for (int j = 0; j < DIMENSION; j++){
+                estadoActual[i][j] = 0;
+            }
+        }
+    }
 }
 
